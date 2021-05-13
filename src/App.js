c865cae382a830a7f3419b0f4e2951e866cb2b0a
Nspired1 <don.spire1@gmail.com>
2021-05-12 21:01:43 -0700

    add Link tag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
