329df1859184932cdad1c86111a0a8fc07791dd8
Nspired1 <don.spire1@gmail.com>
2021-05-12 17:09:51 -0700

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
