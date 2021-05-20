791cbecd1f0ad533587f32863d1e3e540b3dd17c
Nspired1 <don.spire1@gmail.com>
2021-05-19 21:44:28 -0700

    code split routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -16,21 +17,24 @@ const App = () => {
             "url(http://pets-images.dev-apis.com/pets/wallpaperB.jpg)",
         }}
       >
-        <Router>
-          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
-            <Link to="/" className="text-6x1 text-white hover:text-gray-200">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <h2>Test of React Suspense</h2>
+        <Suspense fallback={<h2>Loading Route...</h2>}>
+          <Router>
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+              <Link to="/" className="text-6x1 text-white hover:text-gray-200">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
